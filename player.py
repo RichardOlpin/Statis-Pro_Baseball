d35afcdcd4bfb9722768c3c3a70f6c3fd5b59706
Richard Olpin <richard.olpin@aruplab.com>
2019-08-05 19:30:09 -0600

    Basic player object. populating stats is working

---
+++ player.py
@@ -0,0 +1,65 @@
+import statsapi
+
+
+class Player:
+    def __init__(self, **kwargs):
+        self.id = kwargs['person']['id']
+        self.first_name = kwargs['person']['fullName'].split()[0]
+        self.last_name = kwargs['person']['fullName'].split()[1:]
+        self.position = Position(**kwargs['position'])
+        self.bats = None
+        self.error = None
+        self.obr = None
+        self.sp = None
+        self.hr = None
+        self.cd = None
+        self.sac = None
+        self.inj = None
+        self.single_inf = None
+        self.single7 = None
+        self.single8 = None
+        self.single9 = None
+        self.double7 = None
+        self.double8 = None
+        self.double9 = None
+        self.triple8 = None
+        self.hr = None
+        self.k = None
+        self.bb = None
+        self.hbp = None
+        self.out = None
+        self.bd = {'double': None,
+                   'triple': None,
+                   'hr': None
+                   }
+        self.cd = None
+        self.arm = None
+        self.stats_all = self.get_player_stats(self.id)
+        self.stats_hitting = self.stats_all.get('hitting')
+        self.stats_fielding = self.stats_all.get('fielding')
+        self.stats_pitching = self.stats_all.get('pitching')
+
+    @classmethod
+    def get_player_stats(cls, player_id, group='[hitting,pitching,fielding]',
+                         stat_type='season',
+                         params=None):
+        if not params:
+            params = {'personId': player_id,
+                      'hydrate': f'stats(group={group},type={stat_type}),currentTeam'}
+        results = statsapi.get('person', params)['people'][0]['stats']
+        all_stats = dict()
+        for r in results:
+            stat_type = r['group']['displayName']
+            stats_value = r['splits'][0]['stat']
+            all_stats[stat_type] = stats_value
+        return all_stats
+
+
+
+class Position:
+    def __init__(self, **kwargs):
+        self.code = kwargs['code']
+        self.name = kwargs['name']
+        self.type = kwargs['type']
+        self.abbreviation = kwargs['abbreviation']
+
